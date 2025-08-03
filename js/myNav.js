a41a2aba45ebf5b9eceea5206a4d39f0f01a56da
Cornunicorncrazy <datvupham@gmail.com>
2025-08-03 22:59:57 +0700

    navbar changes
    change navigation animation and customize scroll bar

--- js/myNav.js
+++ js/myNav.js
@@ -1,5 +1,14 @@
 /* Open when someone clicks on the span element */
 function openNav() {
+  // changes styles of the body
+  document.body.style.backgroundColor = "white";
+  document.body.style.overflowY = "hidden"; // Prevents scrolling when the nav is open
+
+  // changes styles of the openNavButton
+  document.getElementById("openNavButton").style.backgroundColor = "transparent";
+  document.getElementById("openNavButton").style.color = "transparent";
+
+  // changes styles of the navigation overlay
   document.getElementById("myNav").style.width = "100%";
   document.getElementById("myNav").style.height = "100%";
   document.getElementById("myNav").style.borderRadius = "0%";
@@ -7,10 +16,20 @@ function openNav() {
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
+  // changes styles of the body
+  document.body.style.backgroundColor = "black";
+  document.body.style.overflowY = "auto"; // Allows scrolling when the nav is closed
+
+  // changes styles of the openNavButton
+  document.getElementById("openNavButton").style.backgroundColor = "black";
+  document.getElementById("openNavButton").style.color = "white";
+
+  // changes styles of the navigation overlay
   document.getElementById("myNav").style.transition = "0.25s";
   document.getElementById("myNav").style.height = "0%";
   document.getElementById("myNav").style.width = "100%";
   document.getElementById("myNav").style.fontSize= "0px";
+  
   setTimeout(() =>  {
   document.getElementById("myNav").style.borderRadius = "100%";
   document.getElementById("myNav").style.transition = "0.4s";
